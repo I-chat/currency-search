fc9919c16cd6e7023a6b9c7ea9ec0a17d79be854
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-10 17:16:16 +0100

    Fix variable error

--- app/search.js
+++ app/search.js
@@ -7,12 +7,14 @@ function QueryService()
   var url = "https://openexchangerates.org/api/latest.json?app_id=7011cbb799504a7391af935ef01020ae";
 
  
-  var client = new XMLHttpRequest();
-  client.open("GET", url, false);
-  client.setRequestHeader("Content-Type", "text/plain");
-  client.send();
-  if (client.status == 200){
-    console.log("Current currency exchange rates are:\n\n" + client.responseText)
+  var guest = new XMLHttpRequest();
+  guest.open("GET", url, false);
+  guest.setRequestHeader("Content-Type", "text/plain");
+  guest.send();
+  var state = guest.status
+  if (state == 200){
+    console.log("Current currency exchange rates are:\n" + guest.responseText);
   }
+};
 
 QueryService()
